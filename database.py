a1960b2f5503672891e213905b200dfd3d864642
adrian <adrianfmc@gmail.com>
2020-07-01 18:37:15 -0500

    2

--- database.py
+++ database.py
@@ -1,34 +1,49 @@
 import sqlite3
 
-conn = sqlite3.connect('customer.db')
 
-#crear cursor
-c = conn.cursor()
+# Query the DB and return all the records
 
-#crear una tabla
-#c.execute("""CREATE TABLE customers (
-#    first_name text,
-#    last_name text,
-#    email text
-#)""")
+def show_all():
+    # Conectar a la base de datos
+    conn = sqlite3.connect('customer.db')
+    # crear cursor
+    c = conn.cursor()
+    # Query database
+    c.execute("SELECT rowid, * FROM customers")
+    items = c.fetchall()
+    for item in items:
+        print(item)
 
-#muchos_customers = [
-#                    ('Momo', 'Espirit', 'momo@espirit.com'),
-#                    ('Zai', 'Espirit', 'zai@espirit.com'),
-#                    ('Adri', 'Fraust', 'adri@fraust.com')
-#                    ]
-#c.executemany("INSERT INTO customers VALUES (?, ?, ?)", muchos_customers)
+#Add new record to the table
+def add_one(first,last,email):
+    conn = sqlite3.connect('customer.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO customers VALUES (?,?,?)", (first,last,email))
+    conn.commit()
+    conn.close()
 
-#Query database
-c.execute("SELECT rowid, * FROM customers")
+def delete_one(id):
+    conn = sqlite3.connect('customer.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM customers WHERE rowid = (?)", id)
+    conn.commit()
+    conn.close()
 
-items = c.fetchall()
+#Add meny new records to the table
+def add_many(list):
+    conn = sqlite3.connect('customer.db')
+    c = conn.cursor()
+    c.executemany("INSERT INTO customers VALUES (?,?,?)", (list))
+    conn.commit()
+    conn.close()
 
-for item in items:
-    print(item)
-
-
-#print("comando ejecutado exitosamente")
-conn.commit()
-
-conn.close()
+# Look up
+def email_lookup(email):
+    conn = sqlite3.connect('customer.db')
+    c = conn.cursor()
+    c.execute("SELECT rowid, *  FROM customers WHERE email = (?)", (email,))
+    items = c.fetchall()
+    for item in items:
+        print(item)
+    conn.commit()
+    conn.close()
---
+++ our_app.py
@@ -0,0 +1,16 @@
+import database
+
+#stuff = [
+#    ('sof','ort','sof@ort.sim'),
+#    ('albert','o','albert@o.com')
+#    ]
+
+#database.add_many(stuff)
+
+# database.delete_one('6')
+
+# database.add_one("Josi","Josefa","josi@josefa.com")
+
+#database.show_all()
+
+database.email_lookup('adri@fraust.com')
